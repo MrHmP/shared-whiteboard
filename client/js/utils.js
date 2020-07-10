b640702a7710dbc3cd83a85e89812b69a2c68dd7
MrHmp <hmp.mcs@gmail.com>
2020-07-10 14:42:07 +0530

    Shortened id length

--- client/js/utils.js
+++ client/js/utils.js
@@ -8,7 +8,7 @@ const MESSAGE_TYPE = {
 }
 
 function uuidv4() {
-    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+    return 'xxxxxxxx'.replace(/[xy]/g, function (c) {
         var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
         return v.toString(16);
     });
--- server/message-consumers/draw.js
+++ server/message-consumers/draw.js
@@ -2,7 +2,7 @@ const state = require('../state');
 const logger = require('./../services/loggerService');
 
 exports.addDrawing = function (board, drawing) {
-    logger.appLog(`Adding board ${JSON.stringify(board)}'s drawing into state`);
+    logger.appLog(`Drawing for board ${board.id}`);
     state.addDrawings(board.id, drawing);
     logger.appLog(`Added drawing into state 👍`);
 }
