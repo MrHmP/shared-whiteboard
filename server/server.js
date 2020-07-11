daa9a62b84092aaf59852e04eb03b15075c3add6
MrHmp <hmp.mcs@gmail.com>
2020-07-11 23:20:13 +0530

    Added nano-id

--- server/server.js
+++ server/server.js
@@ -1,10 +1,5 @@
 const socketService = require('./services/websocketService');
-const state = require('./state');
 const logger = require('./services/loggerService');
-
-function appLog(msg) {
-    console.log(`${new Date()} : ${msg}`);
-}
 
 try {
     socketService.initiateSocketConnection();
--- server/services/loggerService.js
+++ server/services/loggerService.js
@@ -1,13 +1,9 @@
+const nanoid = require('nanoid');
+
 exports.appLog = function (msg) {
     console.log(`${new Date()} : ${msg}`);
 }
 
 exports.getRandomId = function (length) {
-    let result = '';
-    let characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-    let charactersLength = characters.length;
-    for (let i = 0; i < (length || 5); i++) {
-        result += characters.charAt(Math.floor(Math.random() * charactersLength));
-    }
-    return result;
+    return nanoid.random(5);
 }
