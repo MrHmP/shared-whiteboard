01e7efee1026fcb4f067c5888aa886def165352f
Himanshu <hmp.mcs@gmail.com>
2020-09-26 12:15:09 +0530

    Shared board | converted vars to lets in board code

--- client/js/app.js
+++ client/js/app.js
@@ -1,7 +1,7 @@
 /* Canvas */
-var canvas = document.getElementById('drawCanvas');
-var ctx = canvas.getContext('2d');
-var color = document.querySelector(':checked').getAttribute('data-color');
+let canvas = document.getElementById('drawCanvas');
+let ctx = canvas.getContext('2d');
+let color = document.querySelector(':checked').getAttribute('data-color');
 
 canvas.width = Math.min(document.documentElement.clientWidth, window.innerWidth || 300);
 canvas.height = Math.min(document.documentElement.clientHeight, window.innerHeight || 300);
@@ -16,13 +16,13 @@ document.getElementById('colorSwatch').addEventListener('click', function () {
 	color = document.querySelector(':checked').getAttribute('data-color');
 }, false);
 
-var isTouchSupported = 'ontouchstart' in window;
-var isPointerSupported = navigator.pointerEnabled;
-var isMSPointerSupported = navigator.msPointerEnabled;
+let isTouchSupported = 'ontouchstart' in window;
+let isPointerSupported = navigator.pointerEnabled;
+let isMSPointerSupported = navigator.msPointerEnabled;
 
-var downEvent = isTouchSupported ? 'touchstart' : (isPointerSupported ? 'pointerdown' : (isMSPointerSupported ? 'MSPointerDown' : 'mousedown'));
-var moveEvent = isTouchSupported ? 'touchmove' : (isPointerSupported ? 'pointermove' : (isMSPointerSupported ? 'MSPointerMove' : 'mousemove'));
-var upEvent = isTouchSupported ? 'touchend' : (isPointerSupported ? 'pointerup' : (isMSPointerSupported ? 'MSPointerUp' : 'mouseup'));
+let downEvent = isTouchSupported ? 'touchstart' : (isPointerSupported ? 'pointerdown' : (isMSPointerSupported ? 'MSPointerDown' : 'mousedown'));
+let moveEvent = isTouchSupported ? 'touchmove' : (isPointerSupported ? 'pointermove' : (isMSPointerSupported ? 'MSPointerMove' : 'mousemove'));
+let upEvent = isTouchSupported ? 'touchend' : (isPointerSupported ? 'pointerup' : (isMSPointerSupported ? 'MSPointerUp' : 'mouseup'));
 
 canvas.addEventListener(downEvent, startDraw, false);
 canvas.addEventListener(moveEvent, draw, false);
@@ -35,21 +35,21 @@ function drawOnCanvas(color, plots) {
 	ctx.beginPath();
 	ctx.moveTo(plots[0].x, plots[0].y);
 
-	for (var i = 1; i < plots.length; i++) {
+	for (let i = 1; i < plots.length; i++) {
 		ctx.lineTo(plots[i].x, plots[i].y);
 	}
 	ctx.stroke();
 }
 
-var isActive = false;
-var plots = [];
+let isActive = false;
+let plots = [];
 
 function draw(e) {
 	e.preventDefault();
 	if (!isActive) return;
 
-	var x = isTouchSupported ? (e.targetTouches[0].pageX - canvas.offsetLeft) : (e.offsetX || e.layerX - canvas.offsetLeft);
-	var y = isTouchSupported ? (e.targetTouches[0].pageY - canvas.offsetTop) : (e.offsetY || e.layerY - canvas.offsetTop);
+	let x = isTouchSupported ? (e.targetTouches[0].pageX - canvas.offsetLeft) : (e.offsetX || e.layerX - canvas.offsetLeft);
+	let y = isTouchSupported ? (e.targetTouches[0].pageY - canvas.offsetTop) : (e.offsetY || e.layerY - canvas.offsetTop);
 
 	plots.push({ x: (x << 0), y: (y << 0) });
 
